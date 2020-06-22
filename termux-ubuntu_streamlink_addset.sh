fbae7b7826c73924a9e69f30e8a11d6e7ba669a6
jassmusic <59103051+jassmusic@users.noreply.github.com>
2020-06-23 00:38:00 +0900

    Update termux-ubuntu_streamlink_addset.sh

--- termux-ubuntu_streamlink_addset.sh
+++ termux-ubuntu_streamlink_addset.sh
@@ -8,7 +8,7 @@
 
 mv /usr/lib/python2.7/dist-packages/configparser-3.5.0b2-nspkg.pth /home
 #cat /usr/lib/python2.7/dist-packages/backports/__init__.py
-echo `# A Python "namespace package" http://www.python.org/dev/peps/pep-0382/` >> /usr/lib/python2.7/dist-packages/backports/__init__.py
-echo `# This always goes inside of a namespace package's __init__.py` >> /usr/lib/python2.7/dist-packages/backports/__init__.py
-echo `from pkgutil import extend_path` >> /usr/lib/python2.7/dist-packages/backports/__init__.py
-echo `__path__ = extend_path(__path__, __name__)` >> /usr/lib/python2.7/dist-packages/backports/__init__.py
+echo "# A Python "namespace package" http://www.python.org/dev/peps/pep-0382/" >> /usr/lib/python2.7/dist-packages/backports/__init__.py
+echo "# This always goes inside of a namespace package's __init__.py" >> /usr/lib/python2.7/dist-packages/backports/__init__.py
+echo "from pkgutil import extend_path" >> /usr/lib/python2.7/dist-packages/backports/__init__.py
+echo "__path__ = extend_path(__path__, __name__)" >> /usr/lib/python2.7/dist-packages/backports/__init__.py

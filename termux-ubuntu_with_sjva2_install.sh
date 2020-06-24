eade728496e678513aea381595a17200f3823278
jassmusic <59103051+jassmusic@users.noreply.github.com>
2020-06-24 11:56:19 +0900

    Update termux-ubuntu_with_sjva2_install.sh

--- termux-ubuntu_with_sjva2_install.sh
+++ termux-ubuntu_with_sjva2_install.sh
@@ -18,9 +18,10 @@ termux-wake-lock
 sshd
 ~/termux-ubuntu/start-ubuntu.sh
 EOF
+cd ~
 curl -LO https://raw.githubusercontent.com/jassmusic/termux/master/termux-ubuntu_sjva2_install.sh
 mv termux-ubuntu_sjva2_install.sh ~/termux-ubuntu/ubuntu-fs/home
 cat >> ~/termux-ubuntu/ubuntu-fs/root/.bash_profile << EOF
-bash /home/termux-ubuntu_sjva2_install.sh
+bash termux-ubuntu_sjva2_install.sh
 EOF
 ~/termux-ubuntu/start-ubuntu.sh
